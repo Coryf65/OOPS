dbce6acbecf79eb9fc641e939d34b3f32fcf7714
Cory Fabian <Coryf65@users.noreply.github.com>
2023-03-19 21:24:06 -0500

    setup base demo for oo

---
+++ OODemo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Rental agency in florida
+
+Console.WriteLine("Hello, World!");
---
+++ OODemo/RentalCar.cs
@@ -0,0 +1,22 @@
+﻿// Rental agency in florida
+using OODemo;
+
+public class RentalCar
+{
+    public int RentalId { get; set; }
+    public string CurrentRental { get; set; }
+    public decimal PricePerDay { get; set; }
+    public int NumberOfPassengers { get; set; }
+    public CarType CarType { get; set; }
+
+    public void StartEngine()
+    {
+        Console.WriteLine("Turn Key in the ignition");
+        Console.WriteLine("Trun key to on");
+    }
+
+    public void StopEngine()
+    {
+        Console.WriteLine("Turn key to off");
+    }
+}
